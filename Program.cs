7cbf0fcc4ee9a180543063de4418caf186b7429c
mango12322 <xpfl7920@gmail.com>
2026-01-07 13:50:27 +0900

    ConsoleUI add

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // TODO : 타이틀 표시
+            // TODO : 저장된 게임 존재 여부 확인
+            // TODO : 게임 로드 및 새 게임 시작
+            TextRPG.Utils.ConsoleUi.ShowTitle();
         }
     }
 }
---
+++ Utils/ConsoleUi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// 콘솔 관련 UI 유틸리티를 담당
+namespace TextRPG.Utils
+{
+    internal class ConsoleUi
+    {
+        // 타이틀 표시 메서드
+        public static void ShowTitle()
+        {
+                       Console.Clear();
+            Console.WriteLine("===================================");
+            Console.WriteLine("        Welcome to TextRPG        ");
+            Console.WriteLine("===================================");
+            Console.WriteLine();
+        }
+    }
+}
