50c98146c6ba4158d77d9b0f6924099dbdbe8f38
mango12322 <xpfl7920@gmail.com>
2026-01-08 13:48:18 +0900

    기본 추상 클래스 생성

---
+++ Models/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG.Models
+{
+    // 캐릭터 기본 추상 클래스
+    internal abstract class Character
+    {
+        #region 프로퍼티
+        public string Name { get; protected set; }
+        public int CurrentHp { get; protected set; }
+        public int MaxHp { get; protected set; }
+        public int CurrentMp { get; protected set; }
+        public int MaxMp { get; protected set; }
+        public int AttackPower { get; protected set; }
+        public int Defense { get; protected set; }
+        public int Level { get; protected set; }
+
+        public bool IsAlive => CurrentHp > 0;        
+        #endregion
+
+        #region 생성자
+        public Character(string name, int maxHp, int maxMp ,int attackPower, int defense, int level)
+        {
+            Name = name;
+            MaxHp = maxHp;
+            CurrentHp = maxHp;
+            MaxMp = maxMp;
+            CurrentMp = maxMp;
+            AttackPower = attackPower;
+            Defense = defense;
+            Level = level;
+        }
+        #endregion
+
+        #region 메서드
+        #endregion
+    }
+}
